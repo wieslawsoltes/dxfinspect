a0a7cf0ba6c4373c5625a009233074a10645dbdb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-03-03 16:48:07 +0100

    Update Program.cs

--- dxfinspect/Program.cs
+++ dxfinspect/Program.cs
@@ -5,7 +5,7 @@ if (args.Length == 1)
 {
     Console.WriteLine(DxfInspect.ToHtml(args[0]));
 }
-else if (args.Length == 1)
+else if (args.Length == 2)
 {
     DxfInspect.Convert(args[0], args[1]);
 }
