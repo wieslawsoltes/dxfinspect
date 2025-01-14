7c786b738c0d8e7e0f43dda79afa6ed502c0efd9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-01-14 21:41:22 +0100

    Update DxfTreeNodeViewModel.cs

--- dxfInspect.Base/ViewModels/DxfTreeNodeViewModel.cs
+++ dxfInspect.Base/ViewModels/DxfTreeNodeViewModel.cs
@@ -1,22 +1,57 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 using ReactiveUI;
 
 namespace dxfInspect.ViewModels;
 
-public class DxfTreeNodeViewModel(int startLine, int endLine, string code, string data, string type, string nodeKey)
-    : ReactiveObject
+public class DxfTreeNodeViewModel : ReactiveObject
 {
     private bool _isExpanded;
 
-    public string LineNumberRange { get; } = $"{startLine}-{endLine}";
-    public int StartLine { get; } = startLine;
-    public int EndLine { get; } = endLine;
-    public string Code { get; } = code;
-    public string Data { get; } = data;
-    public string Type { get; } = type;
-    public string NodeKey { get; } = nodeKey;
+    public DxfTreeNodeViewModel(int startLine, int endLine, string code, string data, string type, string nodeKey)
+    {
+        StartLine = startLine;
+        EndLine = endLine;
+        Code = code;
+        Data = data;
+        Type = type;
+        NodeKey = nodeKey;
+    }
+
+    public int StartLine { get; }
+    public int EndLine { get; }
+    public string Code { get; }
+    public string Data { get; }
+    public string Type { get; }
+    public string NodeKey { get; }
     public ObservableCollection<DxfTreeNodeViewModel> Children { get; } = [];
     public bool HasChildren => Children.Count > 0;
+
+    public string LineNumberRange
+    {
+        get
+        {
+            if (!HasChildren)
+            {
+                return $"{StartLine}-{EndLine}";
+            }
+            
+            int lastLine = GetLastLineNumber(this);
+            return $"{StartLine}-{lastLine}";
+        }
+    }
+
+    private int GetLastLineNumber(DxfTreeNodeViewModel node)
+    {
+        if (!node.HasChildren)
+        {
+            return node.EndLine;
+        }
+
+        return node.Children
+            .Select(child => GetLastLineNumber(child))
+            .Max();
+    }
 
     public bool IsExpanded
     {
