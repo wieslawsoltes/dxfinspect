0b0a276c49126e8ac6848d720800c8c2b43723da
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-01-14 23:02:13 +0100

    Update theme

--- dxfInspect.Base/Views/DxfMainWindow.axaml.cs
+++ dxfInspect.Base/Views/DxfMainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace dxfInspect.Views;
@@ -18,5 +20,25 @@ public class DxfMainWindow : Window
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+    
+    private void MinimizeWindow(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeWindow(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+    }
+
+    private void CloseWindow(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+    
+    private void BeginWindowDrag(object sender, PointerPressedEventArgs e)
+    {
+        BeginMoveDrag(e);
     }
 }
